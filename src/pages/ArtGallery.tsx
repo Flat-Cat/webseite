058495fcd2cc8e1c11afd31bd1d63ae8eaae6dfe
flat-cat <flat_cat_coding@pm.me>
2023-06-14 13:51:02 +0200

    added bulma card item. added example description

--- src/pages/ArtGallery.tsx
+++ src/pages/ArtGallery.tsx
@@ -6,31 +6,49 @@ function ArtGallery() {
 
   <h1 className="title has-text-black">Art Gallery</h1>
   {[
-    { src: 'bild01.webp', caption: 'Nyam' },
-    { src: 'bild02.webp', caption: 'Astra-ur, mein Freund' },
-    { src: 'bild03.webp', caption: 'Van-gogh-mood' },
-    { src: 'bild10.webp', caption: 'Far away' },
+    { src: 'bild01.webp', caption: 'Nyam', description: 'Aquarelle, Fineliner'},
+    { src: 'bild02.webp', caption: 'Astra-ur, mein Freund', description: 'Prost. Gouache' },
+    { src: 'bild03.webp', caption: 'Van-gogh-mood', description: 'Gouache' },
+    { src: 'bild10.webp', caption: 'Far away', description: 'Gouache, Buntstifte, Fineliner' },
 
-    { src: 'bild06.webp', caption: 'Dreamy.Mode=on' },
-    { src: 'bild07.webp', caption: 'UglyUs' },
-    { src: 'bild08.webp', caption: 'Uga Buga' },
-    { src: 'bild09.webp', caption: 'Melty Miau' },
-    { src: 'bild04.webp', caption: 'Meeres-Miau' },
+    { src: 'bild06.webp', caption: 'Dreamy.Mode=on', description: 'Aquarelle, acryl' },
+    { src: 'bild07.webp', caption: 'UglyUs', description: 'Buntstifte' },
+    { src: 'bild08.webp', caption: 'Uga Buga', description: 'Gouache, Stifte' },
+    { src: 'bild09.webp', caption: 'Melty Miau', description: 'Alkohol Ink' },
+    { src: 'bild04.webp', caption: 'Meeres-Miau', description: 'Gouache' },
 
-    { src: 'bild11.webp', caption: 'Philodentron' },
-    { src: 'bild12.webp', caption: 'Kinky King' },
-    { src: 'bild13.webp', caption: 'Us' },
-    { src: 'bild14.webp', caption: 'Planty Handy' },
-    { src: 'bild05.webp', caption: 'it tikles!' },
+    { src: 'bild11.webp', caption: 'Philodentron', description: 'Gouache, Aquarelle' },
+    { src: 'bild12.webp', caption: 'Kinky King', description: 'Gouache' },
+    { src: 'bild13.webp', caption: 'Us', description: 'Aquarelle, Fineliner' },
+    { src: 'bild14.webp', caption: 'Planty Handy', description: 'Aquarelle' },
+    { src: 'bild05.webp', caption: 'it tikles!', description: 'Gouache' },
     ].map((item, index) => (
     
     <div className="gallery-item" key={index}>
 
       <figure>
         <img src={`img/artgallery/thumbnails/${item.src}`}/>
-        <figcaption>{item.caption}</figcaption>
+        {/* <figcaption>{item.caption}</figcaption> */}
       </figure>
 
+      <div className="card">
+  <div className="card-image">
+
+  </div>
+  <div className="card-content">
+    <div className="media">
+      <div className="media-left">
+
+      </div>
+      <div className="media-content">
+
+        <p className="subtitle is-6">{item.caption}</p>
+      </div>
+    </div>
+
+    <div className="content">{item.description}</div>
+  </div>
+</div>
     </div>
   ))}
 </div>
